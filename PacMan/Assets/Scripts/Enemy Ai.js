3f9134cff2092496299fb12f303bc043b79672ba
bargainbenny <owen.charland@gmail.com>
2015-05-22 22:19:17 -0700

    sync 1
    Added level
    Ghost
    Fog of war
    Scripting

---
+++ PacMan/Assets/Scripts/Enemy Ai.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+public var target: Transform;
+public var moveSpeed = 3;
+public var rotationSpeed = 3;
+public var myTransform: Transform;
+
+function Awake()
+{
+	myTransform = transform;
+}
+
+function Start () 
+{
+	target = GameObject.FindWithTag("Player").transform;
+}
+
+function Update () 
+{
+	var lookDir = target.position - myTransform.position;
+	lookDir.y = 0;
+	myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(lookDir), rotationSpeed*Time.deltaTime);
+	myTransform.position += myTransform.forward * moveSpeed*Time.deltaTime;
+}
---
+++ PacMan/Assets/Scripts/scoring.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+public var scoreText = "";
+public var score = 0;
+
+public var go : GameObject;
+
+function Start () {
+	
+	score = 21;
+	scoreText = "Score: " + score;
+
+}
+
+function OnTriggerEnter(other : Collider)
+{
+	if(other.tag == "C4Item")
+	{
+		score -= 1;
+		Destroy(other.gameObject);
+	}
+}
+
+function Update () {
+	
+	scoreText = "Score: " + score;
+}
+
+function OnGUI()
+{
+	GUI.Box(new Rect(10, 10, 100, 20),scoreText);
+}
