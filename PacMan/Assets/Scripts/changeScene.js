ce13712fee994ce261d266a41c76287733cfb751
bargainbenny <owen.charland@gmail.com>
2015-05-26 07:46:24 -0700

    added stuff

---
+++ PacMan/Assets/Scripts/changeScene.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	
+	if(1==1)
+	{
+		
+		Application.LoadLevel("level02");
+	}
+	
+}
--- PacMan/Assets/Scripts/scoring.js
+++ PacMan/Assets/Scripts/scoring.js
@@ -1,14 +1,14 @@
 ﻿#pragma strict
 
-public var scoreText = "";
-public var score = 0;
+public var itemsLeftText = "";
+public var itemsLeft = 0;
 
 public var go : GameObject;
 
 function Start () {
 	
-	score = 21;
-	scoreText = "Score: " + score;
+	itemsLeft = 185;
+	itemsLeftText = itemsLeft + " Packets Left!";
 
 }
 
@@ -16,17 +16,17 @@ function OnTriggerEnter(other : Collider)
 {
 	if(other.tag == "C4Item")
 	{
-		score -= 1;
+		itemsLeft -= 1;
 		Destroy(other.gameObject);
 	}
 }
 
 function Update () {
 	
-	scoreText = "Score: " + score;
+	itemsLeftText = itemsLeft + " Packets Left!";
 }
 
 function OnGUI()
 {
-	GUI.Box(new Rect(10, 10, 100, 20),scoreText);
+	GUI.Box(new Rect(10, 10, 100, 20),itemsLeftText);
 }
